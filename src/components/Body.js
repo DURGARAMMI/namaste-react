d84a1cfd7c60576c4b4722c025bed92df6da1ebf
rameshd <rameshd@turito.com>
2024-02-01 16:49:28 +0530

    HOok

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData.js";
+import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+  console.log(listOfRestaurants);
+  //   let listOfRestaurants = [];
   return (
     <div className="body">
-      <div className="search">Search</div>
+      {/* <div className="search">Search</div> */}
+      <div className="filter">
+        <button
+          className="top-rated-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setlistOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
       <div className="res-container">
         {/* <RestaurantsComp resName="Meghana Foods" cuisines="Asian, Fast Food" /> */}
         {/* <RestaurantsComp resData={resList[0]} />
           <RestaurantsComp resData={resList[1]} />
           <RestaurantsComp resData={resList[2]} /> */}
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
